5c69d0caabad215a6255c603f4eb215ad9529ae3
Daniel <itsdaniel@protonmail.com>
2019-05-11 15:02:12 +0200

    dodalem pojo user

---
+++ src/main/java/com/wwsis/miniblog/model/User.java
@@ -0,0 +1,59 @@
+package com.wwsis.miniblog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @Column(name="user_id")
+    private int user_id;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    public User() {
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
