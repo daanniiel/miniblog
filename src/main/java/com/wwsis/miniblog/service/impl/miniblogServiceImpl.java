0d3363d259521cd9787c7876b04fbcd93d5effed
Kamil Kubiak <voxu94@gmail.com>
2019-06-23 10:27:41 +0200

    implementacja miniblogService

---
+++ src/main/java/com/wwsis/miniblog/service/impl/miniblogServiceImpl.java
@@ -0,0 +1,17 @@
+package com.wwsis.miniblog.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.wwsis.miniblog.dao.FollowerDao;
+import com.wwsis.miniblog.dao.MessageDao;
+import com.wwsis.miniblog.dao.UserDao;
+import com.wwsis.miniblog.model.Follower;
+import com.wwsis.miniblog.model.Message;
+import com.wwsis.miniblog.model.User;
+import com.wwsis.miniblog.service.miniblogService;
+
+@Service
+public class miniblogServiceImpl implements miniblogService{
+
+}
