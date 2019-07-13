51f4ed6da99240fb6956049b0b8c23b0cd20632d
Grzesiekmak <49204318+Grzesiekmak@users.noreply.github.com>
2019-07-13 14:10:48 +0200

    Update MiniblogServiceImpl.java

--- src/main/java/com/wwsis/miniblog/service/impl/MiniblogServiceImpl.java
+++ src/main/java/com/wwsis/miniblog/service/impl/MiniblogServiceImpl.java
@@ -1,7 +1,9 @@
 package com.wwsis.miniblog.service.impl;
-
+ 
+import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import com.wwsis.miniblog.dao.FollowerDao;
 import com.wwsis.miniblog.dao.MessageDao;
@@ -9,59 +11,81 @@ import com.wwsis.miniblog.dao.UserDao;
 import com.wwsis.miniblog.model.Message;
 import com.wwsis.miniblog.model.User;
 import com.wwsis.miniblog.service.MiniblogService;
-
+import org.springframework.transaction.annotation.Transactional;
+ 
 @Service
-public class MiniblogServiceImpl implements MiniblogService {
-
+ 
+public class miniblogServiceImpl implements MiniblogService {
+ 
+ 
+    @Qualifier("followerDaoImpl")
     @Autowired
-    MessageDao messageDao;
-
+    private FollowerDao followerDao;
+ 
+    @Qualifier("MessageDao")
     @Autowired
-    UserDao userDao;
-
+    private MessageDao messageDao;
+ 
+    @Qualifier("UserDao")
     @Autowired
-    FollowerDao followerDao;
-
-    @Override
-    public void follow(User follower, User followee) {
-        followerDao.follow(follower,followee);
-    }
-
-    @Override
-    public void unfollow(User follower, User followee) {
-        followerDao.unfollow(follower,followee);
-    }
-
-    @Override
-    public boolean isFollowing(User follower, User followee) {
-        return followerDao.isFollowing(follower,followee);
-    }
-
+    private UserDao userDao;
+ 
+ 
     @Override
     public List<Message> getUserMessages(User user) {
-        return messageDao.getUserMessages(user);
+ 
+        List<Message> messages = messageDao.getUserMessages(user);
+ 
+        Collections.reverse(messages);
+ 
+        return messages;
     }
-
+ 
     @Override
     public List<Message> getTimelineMessages(User user) {
-        return messageDao.getTimelineMessages(user);
+ 
+        List<Message> messages = messageDao.getTimelineMessages(user);
+ 
+        Collections.reverse(messages);
+ 
+        return messages;
     }
-
+ 
     @Override
     public List<Message> getAllMessages() {
-        return messageDao.getAllMessages();
+ 
+        List<Message> messages = messageDao.getAllMessages();
+ 
+        Collections.reverse(messages);
+ 
+        return messages;
     }
-
+ 
     @Override
     public void addMessage(Message message) {
         messageDao.addMessage(message);
     }
-
+ 
+    @Override
+    public void follow(User follower, User followee) {
+        followerDao.follow(follower,followee);
+    }
+ 
+    @Override
+    public void unfollow(User follower, User followee) {
+        followerDao.unfollow(follower,followee);
+    }
+ 
+    @Override
+    public boolean isFollowing(User follower, User followee) {
+        return followerDao.isFollowing(follower,followee);
+    }
+ 
     @Override
     public User getUser(String name) {
         return userDao.getUser(name);
     }
-
+ 
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
